93ec713b6b0622155f4e1a2234898a8d5c20a4d4
y11n <samtori3849@gmail.com>
2025-10-19 22:00:49 +0900

    feat(model): 커스텀 구분자로 숫자 추출하는 기능 구현

--- src/main/java/calculator/model/Splitter.java
+++ src/main/java/calculator/model/Splitter.java
@@ -2,6 +2,8 @@ package calculator.model;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Splitter {
     private final String BASIC_DELIMITER = ",|:";
@@ -10,11 +12,26 @@ public class Splitter {
         if(input == null || input.isEmpty()){
             return List.of("0");
         }
-        return splitWithBasicDelimiter(input);
+        if (input.startsWith("//")) {
+            return splitWithCustomDelimiter(input);
+        } else {
+            return splitWithBasicDelimiter(input);
+        }
     }
 
     private List<String> splitWithBasicDelimiter(String input){
         return Arrays.asList(input.split(BASIC_DELIMITER));
     }
 
+    private List<String> splitWithCustomDelimiter(String input) {
+        Pattern pattern = Pattern.compile("//(.*)\\\\n(.*)");
+        Matcher matcher = pattern.matcher(input);
+
+        if(!matcher.matches()){
+            throw new IllegalArgumentException("잘못된 커스텀 구분자 형식입니다.");
+        }
+        String delimiter = matcher.group(1);
+        String numbers = matcher.group(2);
+        return Arrays.asList(numbers.split(delimiter));
+    }
 }
